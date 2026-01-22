b3d738a82b3a07afef3d48f7f7e13d5036d96ad6
MethukumelliGithika <githikamethukumelli7@gmail.com>
2026-01-22 23:06:39 +0530

    Day 1: Java introduction and first program

---
+++ PrintDetails.java
@@ -0,0 +1,7 @@
+public class PrintDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: Githika");
+        System.out.println("Course: Java Programming");
+        System.out.println("Day 1: Introduction to Java");
+    }
+}
